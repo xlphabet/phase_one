815a09b42fda31df5bbf97c55baad0c0cf8b46e6
kiran <xlphabet@gmail.com>
2017-10-13 14:05:46 -0600

    Hello World, Suck My Ballsgit add Hello.java

---
+++ src/Hello.java
@@ -0,0 +1,13 @@
+
+public class Hello {
+	//Hello is a class name
+	
+	public static void main(String[] args) {
+		//Main is a Special Method, Program starts executing from Main
+		System.out.println("Hello, World!!");
+		System.out.println(("Suck My Balls"));
+		//System.out.println is a method from java libraries
+	}
+
+}
+//Semantics - Meaning of the Program.
